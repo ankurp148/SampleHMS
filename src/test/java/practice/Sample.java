cb11b5e0907ac3cac341e1401ad274488fdcbd63
ankur <ankurp1481995@gmail.com>
2024-03-06 12:29:55 +0530

    this is commit msg

---
+++ src/test/java/practice/Sample.java
@@ -0,0 +1,36 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test (groups = "system")
+	
+	public void method3_Test()
+	{
+		System.out.println("====TEST SCRIPT 03====");
+	}
+	
+	@Test (groups = {"regression","smoke"})
+	
+	public void method4_Test()
+	{
+		System.out.println("====TEST SCRIPT 04====");
+	}
+	
+	@Test(groups = "smoke")
+	
+	public void method5_Test()
+	{
+		System.out.println("====TEST SCRIPT 05====");
+	}
+	
+	@Test (groups = "system")
+	
+	public void method6_Test()
+	{
+		System.out.println("====TEST SCRIPT 06====");
+	}
+
+
+
+}
