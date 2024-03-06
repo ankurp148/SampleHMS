478a8528d471dbbb0afbf27761ee67058282042f
ankur <ankurp1481995@gmail.com>
2024-03-06 13:20:09 +0530

    new TS added

---
+++ src/test/java/Demo/Demo.java
@@ -0,0 +1,21 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test(groups = "smoke")
+	
+	public void method1_Test()
+	{
+		System.out.println("====TEST SCRIPT 11====");
+	}
+	
+	@Test(groups = "system")
+	
+	public void method2_Test()
+	{
+		System.out.println("====TEST SCRIPT 12====");
+	}
+
+}
